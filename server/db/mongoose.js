fc3758f4f2daa715418aa483d48c2e57fba2c4f1
Abubakar Akram <pipsonion2010@gmail.com>
2018-07-30 04:12:42 +0500

    Setup todo-api for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/todos');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todos');
 
 
 module.exports = {mongoose};
